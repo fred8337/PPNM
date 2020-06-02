f4718a2eb0a8cfb95f4c5d3e8f651ba11e7d3470
fred8337 <fred8337@DESKTOP-FACE7G3.localdomain>
2020-06-02 11:11:20 +0200

    Exam Commit

--- Minimization/main.cs
+++ Minimization/main.cs
@@ -1,5 +1,66 @@
 using System;
+using System.IO;
+using System.Linq;
 public static class main
 {
-    
+    static Func<vector, double> rosenbrock = delegate(vector x)
+    {
+        double result = Math.Pow(1-x[0], 2)+100*Math.Pow((x[1]-Math.Pow(x[0], 2)), 2);
+        return result;
+    };
+    static Func<vector, double> himmelblau = delegate(vector x)
+    {
+        double result = Math.Pow(Math.Pow(x[0], 2)+x[1]-11, 2)+Math.Pow(x[0]+Math.Pow(x[1], 2)-7, 2);
+        return result;
+    };
+    static Func<vector, double> breitWigner = delegate(vector x)
+    {
+        double result = x[3]/(Math.Pow(x[0]-x[1],2)+Math.Pow(x[2],2)/4);
+        return result;
+    };
+    static Func<vector, double> deviation = delegate(vector x)
+    {
+        double res = 0;
+        int n = Es.Length;
+        for(int i = 0;i<n;i++)
+        {
+            res+=Math.Pow(breitWigner(new vector(new double[]{Es[i], x[0], x[1], x[2]}))-Crosss[i], 2);
+        }
+        return res;
+    };
+    public static double[] Es = new double[100];
+    public static double[] Crosss = new double[100];
+    public static double[] Errors = new double[100];
+    public static void Main()
+    {
+        vector guesses = new vector(new double[]{0, 0});
+        int before = qnewton.steps;
+        var res = qnewton.QNewton(rosenbrock, guesses, 0.000001);
+        int after = qnewton.steps;
+        res.print($"Found minimum of Rosenbrock in {after-before} steps: ");
+        before = qnewton.steps;
+        var res2 = qnewton.QNewton(himmelblau, guesses, 0.000001);
+        after = qnewton.steps;
+        res2.print($"Found minimum of Himmelblau in {after-before} steps: ");
+
+        var data = File.ReadAllLines("/home/fred8337/PPNM/PPNM/Minimization/data.txt");
+        var size = data.Length;
+        
+        var splitted_data = data.Select(x => x.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries));
+
+        int i = 0;
+        foreach (var line in splitted_data)
+        {   
+            // Console.WriteLine(line[1]);
+            Es[i]=double.Parse(line[0]);
+            Crosss[i]=double.Parse(line[1]);
+            Errors[i]=double.Parse(line[2]);
+        }
+        
+        guesses = new vector(new double[]{125, 0.005, 1});
+        before = qnewton.steps;
+        var res3 = qnewton.QNewton(deviation, guesses, 0.00001);
+        after = qnewton.steps;
+        res3.print($"Found minimum of Deviation in {after-before} steps: ");
+    }
 }
--- Minimization/qnewton.cs
+++ Minimization/qnewton.cs
@@ -1,7 +1,64 @@
+using System;
 public static class qnewton
 {
-    public static vector qnewton(Func<vector,double> f, vector xstart, double eps)
+    public static int steps = 0;
+
+    // Borrowed from Dmitri
+    public static readonly double EPS=1.0/4194304;
+    public static vector gradient(Func<vector,double>f, vector x){
+        vector g=new vector(x.size);
+        double fx=f(x);
+        for(int i=0;i<x.size;i++){
+            double dx=Math.Abs(x[i])*EPS;
+            if(Math.Abs(x[i])<Math.Sqrt(EPS)) dx=EPS;
+            x[i]+=dx;
+            g[i]=(f(x)-fx)/dx;
+            x[i]-=dx;
+        }
+        return g;
+    }
+    public static vector QNewton(Func<vector,double> f, vector xstart, double eps)
     {
-        
+        int n = xstart.size;
+        matrix H = new matrix(n, n);
+        H.set_unity();
+        var x = xstart;
+        double fx;
+        double fy;
+        vector y;
+        while(true)
+        {
+            steps++;
+            vector grad = gradient(f, x);
+            var dir = -H*grad;
+            fx = f(x);
+            double lamb=2;
+            vector deltaX;
+            while(true)
+            {
+                lamb/=2;
+                deltaX = lamb*dir;
+                y=x+deltaX;
+                fy = f(y);
+                if(lamb<0.02)
+                {
+                    H.set_unity();
+                }
+                if(fy<fx+0.0001*deltaX.dot(grad)){break;}
+            }
+            x = y;
+            fx=fy;
+            if(deltaX.norm()<eps){break;}
+            
+            vector gradForUpdate = gradient(f, x);
+            var yk = gradForUpdate-grad;
+            var vecForUpdate = deltaX-H*yk;
+            double denom = (vecForUpdate.dot(yk));
+            if(denom>eps)
+            {
+                H = H + (matrix.outer(vecForUpdate, vecForUpdate))/denom;
+            }
+        }
+        return x;
     }
 }
